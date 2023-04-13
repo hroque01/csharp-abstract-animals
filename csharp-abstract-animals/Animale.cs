6d2ef81941dc1b422097dd9758390927408a2aa8
Hadson Roque <nineonezero.light@gmail.com>
2023-04-13 15:35:39 +0200

    Create abstract class Animale with subclasses

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    abstract class Animale
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("ZZZZZZ");
+        }
+        public abstract void Verso();
+        public abstract void Mangia();
+    }
+
+    class Cane : Animale 
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Bau");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("croccantini");
+        }
+    }
+
+    class Passerotto : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("cip cip");
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("Semi");
+        }
+    }
+
+    class Aquila : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("screech");
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("carne");
+        }
+    }
+
+    class Delfino : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("siuumm");
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("sardine");
+        }
+    }
+}
--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Creiamo quindi una classe astratta Animale e delle classi che la estendono : Cane, Passerotto, Aquila, Delfino
+            Vogliamo che gli animali abbiano i seguenti metodi
+            void Dormi() (mostra a video “Zzz”)
+            void Verso() (mostra a video il verso fatto dall'animale specifico)
+            void Mangia() (mostra a video quello che mangia: erba, carne, ...?)
+            Quali di questi metodi devono essere implementati nella classe astratta e quali devono essere lasciati astratti e implementati dalle classi che la estendono?
+            Una volta create le classi, nel programma istanziare alcuni animali e utilizzare i metodi implementati.*/
+
         }
     }
 }
